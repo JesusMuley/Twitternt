1afa6c8c518e9b39fe9e224d9333cba1818eeaf4
JesusMuley <0619587825@uma.es>
2019-05-06 18:19:57 +0200

    gitignore arreglado full

---
+++ Twitternt-ejb/src/java/twitternt/dao/GrupoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitternt.dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import twitternt.entity.Grupo;
+
+/**
+ *
+ * @author Jesús Muley
+ */
+@Stateless
+public class GrupoFacade extends AbstractFacade<Grupo> {
+
+    @PersistenceContext(unitName = "Twitternt-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public GrupoFacade() {
+        super(Grupo.class);
+    }
+    
+}
---
+++ Twitternt-ejb/src/java/twitternt/entity/Grupo.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitternt.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Jesús Muley
+ */
+@Entity
+@Table(name = "grupo")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Grupo.findAll", query = "SELECT g FROM Grupo g")
+    , @NamedQuery(name = "Grupo.findById", query = "SELECT g FROM Grupo g WHERE g.id = :id")
+    , @NamedQuery(name = "Grupo.findByNombre", query = "SELECT g FROM Grupo g WHERE g.nombre = :nombre")
+    , @NamedQuery(name = "Grupo.findByDescripcion", query = "SELECT g FROM Grupo g WHERE g.descripcion = :descripcion")})
+public class Grupo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "Nombre")
+    private String nombre;
+    @Size(max = 100)
+    @Column(name = "Descripcion")
+    private String descripcion;
+    @JoinTable(name = "grupo_usuarios", joinColumns = {
+        @JoinColumn(name = "grupo", referencedColumnName = "ID")}, inverseJoinColumns = {
+        @JoinColumn(name = "usuario", referencedColumnName = "id")})
+    @ManyToMany
+    private List<Usuario> usuarioList;
+    @JoinColumn(name = "admin", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Usuario admin;
+
+    public Grupo() {
+    }
+
+    public Grupo(Integer id) {
+        this.id = id;
+    }
+
+    public Grupo(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @XmlTransient
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    public Usuario getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Usuario admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Grupo)) {
+            return false;
+        }
+        Grupo other = (Grupo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "twitternt.entity.Grupo[ id=" + id + " ]";
+    }
+    
+}
